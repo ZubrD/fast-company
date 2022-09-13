60f167c017ee7a02624cd34da1bf5c757cdf83f4
Денис Зубрицкий <fort_zd@mail.ru>
2022-09-13 15:24:14 +0700

    Привёл в соответствие с правильным ответом

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Users from './components/users';
+import reportWebVitals from "./reportWebVitals";
 import 'bootstrap/dist/css/bootstrap.css'
+import Users from './app/components/users';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Users/>
+  <React.StrictMode>
+    <Users />
+  </React.StrictMode>
 );
 
+reportWebVitals();
